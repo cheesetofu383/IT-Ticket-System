3cb4de60c62040b98b9ccfb67bee71bb763eb278
Cheesetofu <xavier@innocom.com.sg>
2026-09-16 10:43:00 +0800

    Render

---
+++ public/script.js
@@ -0,0 +1,7 @@
+function loginAsCustomer() {
+    window.location.href = "/customer.html";
+}
+
+function loginAsStaff() {
+    window.location.href = "/staff.html";
+}
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -11,6 +10,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`IT Ticketing System running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`IT Ticketing System running on port ${PORT}`);
 });
